e9138260d77e699de62c5587f95e612c346c5ee4
Kamil <kamil.support@gmail.com>
2024-11-26 14:26:16 +0100

    1

--- 5_RE/5_sub_split.py
+++ 5_RE/5_sub_split.py
@@ -4,3 +4,8 @@ text = "Moje numery telefonu to 123-456-7890 oraz 098-765-4321."
 pattern = r'\d'
 new_text = (re.sub(pattern, '#', text))
 print(new_text)
+
+text = "Python, Java; C++|JavaScript"
+pattern = r'[,;|\s]+'
+parts = re.split(pattern, text)
+print(parts)
