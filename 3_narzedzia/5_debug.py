e884391ef0c5808b5b2bf97c34038bb1abb455ac
Kamil <kamil.support@gmail.com>
2024-11-29 15:10:56 +0100

    '1'

--- 3_narzedzia/5_debug.py
+++ 3_narzedzia/5_debug.py
@@ -2,6 +2,8 @@ def factorial(n):
     if n < 0:
         raise ValueError("Liczba musi być nieujemna")
     result = 1
+    import ipdb
+    ipdb.set_trace()
     for i in range(1, n + 1):
         result *= i
     return result
